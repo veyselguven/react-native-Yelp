feb30933040b8aa4f3b52c98f71ab92545cd1516
veyselguven <vgbasbaydar@gmail.com>
2023-07-17 22:23:18 -0700

    added search screen

---
+++ src/components/SearchBar.js
@@ -0,0 +1,13 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+
+const SearchBar = () => {
+  return (
+    <View>
+      <Text>SearchBar</Text>
+    </View>
+  );
+};
+const styles = StyleSheet.create({});
+
+export default SearchBar;
